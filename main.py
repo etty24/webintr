a0bde12fd3d40b940035efe555f97fa53d99afe7
etty24 <etycsndra@gmail.com>
2024-03-04 14:17:32 +0900

    r1

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd 
+#import matplotlib.pyplot as plt
+
+st.write("coding club 2024 ")
+df = pd.DataFrame({
+    'No':[1,2,3,4],
+    'Nama' :['ety','kia','pute','anita'],
+    'nilai':[91,89,80,100]
+
+})
+
+df
